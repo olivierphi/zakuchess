372f18c89c7c2ad4ff4edf9ee362fcc4d25c9d48
Olivier Philippon <olivier@dunsap.com>
2023-09-03 18:21:13 +0100

    Look for both year-month-day specific games, but also for month-day ones

--- src/apps/chess/business_logic/_get_current_daily_challenge.py
+++ src/apps/chess/business_logic/_get_current_daily_challenge.py
@@ -10,10 +10,20 @@ def get_current_daily_challenge() -> "DailyChallenge":
     from ..models import DailyChallenge
 
     today = timezone.now().date()
-    today_str = today.strftime("%Y-%m-%d")
+    today_as_strings = (
+        # We may have a challenge for this specific day of this specific year:
+        today.strftime("%Y-%m-%d"),
+        # If not, we'll look for a challenge for this specific day of any year:
+        today.strftime("%m-%d"),
+    )
 
-    try:
-        return DailyChallenge.objects.get(id=today_str)
-    except DailyChallenge.DoesNotExist:
-        # TODO: implement multiple fallback daily challenges (and use a modulo to select one)
-        return DailyChallenge.objects.get(id="fallback")
+    for today_str in today_as_strings:
+        # N.B. On a non-SQLite database we would have tried to use a single query
+        # to fetch both cases, but n+1 queries are not a problem with SQlite.
+        try:
+            return DailyChallenge.objects.get(id=today_str)
+        except DailyChallenge.DoesNotExist:
+            pass
+
+    # TODO: implement multiple fallback daily challenges (and use a modulo to select one)
+    return DailyChallenge.objects.get(id="fallback")
