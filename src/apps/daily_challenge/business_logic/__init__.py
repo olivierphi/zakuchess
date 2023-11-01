5edf7caf81b23f8bf9545668950f994853b76987
Olivier Philippon <olivier@dunsap.com>
2023-11-01 21:50:39 +0000

    [refacto] Extract the "speech bubble" logic to its own module
    It grew quite big, so it's time to move this code to its own realm :-)

--- src/apps/daily_challenge/business_logic/__init__.py
+++ src/apps/daily_challenge/business_logic/__init__.py
@@ -2,6 +2,7 @@
 from ._compute_fields_before_bot_first_move import compute_fields_before_bot_first_move
 from ._get_current_daily_challenge import get_current_daily_challenge
 from ._get_daily_challenge_turns_state import get_daily_challenge_turns_state
+from ._get_speech_bubble import get_speech_bubble
 from ._move_daily_challenge_piece import move_daily_challenge_piece
 from ._set_daily_challenge_teams_and_pieces_roles import (
     set_daily_challenge_teams_and_pieces_roles,
